553a9102c745813e4c8e649d175404dbcc78ec7d
Celso Seron Júnior <celso.seron@bscm.com.br>
2021-03-13 20:20:15 -0300

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/example/myUI5App/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.example.myUI5App.controller.MainView", {});
+});
