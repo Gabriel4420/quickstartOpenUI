b98ac4c04fc3e410e3f7c913d82061cb3030176f
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2021-03-17 10:46:09 -0300

    feat: :zap:

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,7 +1,24 @@
 sap.ui.define([
-  "com/example/myUI5App/controller/BaseController"
-], function(Controller) {
+  "com/example/myUI5App/controller/BaseController",
+  "sap/ui/model/json/JSONModel"
+], function (BaseController,
+  JSONModel
+  ) {
   "use strict";
 
-  return Controller.extend("com.example.myUI5App.controller.MainView", {});
+  return BaseController.extend("com.example.myUI5App.controller.MainView", {
+
+    onInit: function () {
+
+      var oView = this.getView();
+
+      $.get('https://jsonplaceholder.typicode.com/todos').done(function (oResponse) {       
+        var oModel = new JSONModel();
+        oModel.setData(oResponse);
+        oView.setModel(oModel);
+      });
+
+    }
+
+  });
 });
